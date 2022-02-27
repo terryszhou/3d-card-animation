03a6df46c7cc0c87cec10cedb70bb56ba018fe7b
terryszhou <terryszhou@gmail.com>
2022-02-27 14:10:33 -0600

    Basic animations added

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // Movement Animation to Happen
 var card = document.querySelector(".card");
 var container = document.querySelector(".container");
-// Movement Animation Eventid;
+// Movement Animation Eventid
 container.addEventListener("mousemove", function (e) {
-    var xAxis = (window.innerWidth / 2 - e.pageX);
-    var yAxis = (window.innerWidth / 2 - e.pageY);
-    card.style.transform = "rotateY(".concat(yAxis, "deg) rotatex(").concat(xAxis, "deg)");
+    var xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+    var yAxis = (window.innerWidth / 2 - e.pageY) / 25;
+    card.style.transform = "rotateY(".concat(xAxis, "deg) rotateX(").concat(yAxis, "deg)");
+});
+// Animate In
+container.addEventListener("mouseenter", function (e) {
+    card.style.transition = "none";
+});
+// Animate Out
+container.addEventListener("mouseleave", function (e) {
+    card.style.transition = "all 0.5s ease";
+    card.style.transform = "rotateY(0deg) rotateX(0deg)";
 });
--- app.ts
+++ app.ts
@@ -2,11 +2,22 @@
 const card: HTMLElement = document.querySelector<HTMLElement>(".card")!;
 const container: HTMLElement = document.querySelector<HTMLElement>(".container")!;
 
-// Movement Animation Eventid;
-container.addEventListener("mousemove", (e: MouseEvent) => {
-  let xAxis: number = (window.innerWidth / 2 - e.pageX);
-  let yAxis: number = (window.innerWidth / 2 - e.pageY);
-  card.style.transform = `rotateY(${yAxis}deg) rotatex(${xAxis}deg)`;
+// Movement Animation Eventid
+container.addEventListener("mousemove", (e: MouseEvent): void => {
+  let xAxis: number = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis: number = (window.innerWidth / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+// Animate In
+container.addEventListener("mouseenter", (e: MouseEvent): void => {
+  card.style.transition = "none";
+});
+
+// Animate Out
+container.addEventListener("mouseleave", (e: MouseEvent): void => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 });
 
 
