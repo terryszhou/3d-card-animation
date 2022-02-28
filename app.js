c4aed8eba5f122a273734dd0649ed1e069ca44f8
terryszhou <terryszhou@gmail.com>
2022-02-27 18:04:15 -0600

    MVP complete, bugginess on different browsers

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // Movement Animation to Happen
 var card = document.querySelector(".card");
 var container = document.querySelector(".container");
+// Items
+var title = document.querySelector(".title");
+var sneaker = document.querySelector(".sneaker img");
+var purchase = document.querySelector(".purchase");
+var description = document.querySelector(".info h3");
+var sizes = document.querySelector(".sizes");
 // Movement Animation Eventid
 container.addEventListener("mousemove", function (e) {
     var xAxis = (window.innerWidth / 2 - e.pageX) / 25;
@@ -10,9 +16,21 @@ container.addEventListener("mousemove", function (e) {
 // Animate In
 container.addEventListener("mouseenter", function (e) {
     card.style.transition = "none";
+    // Popout
+    title.style.transform = "translateZ(200px)";
+    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+    description.style.transform = "translateZ(125px)";
+    sizes.style.transform = "translateZ(100px)";
+    purchase.style.transform = "translateZ(250px)";
 });
 // Animate Out
 container.addEventListener("mouseleave", function (e) {
     card.style.transition = "all 0.5s ease";
     card.style.transform = "rotateY(0deg) rotateX(0deg)";
+    // Snapback
+    title.style.transform = "translateZ(0px)";
+    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+    description.style.transform = "translateZ(0px)";
+    sizes.style.transform = "translateZ(0px)";
+    purchase.style.transform = "translateZ(0px)";
 });
--- app.ts
+++ app.ts
@@ -2,6 +2,13 @@
 const card: HTMLElement = document.querySelector<HTMLElement>(".card")!;
 const container: HTMLElement = document.querySelector<HTMLElement>(".container")!;
 
+// Items
+const title: HTMLElement = document.querySelector(".title")!;
+const sneaker: HTMLElement = document.querySelector(".sneaker img")!;
+const purchase: HTMLElement = document.querySelector(".purchase")!;
+const description: HTMLElement = document.querySelector(".info h3")!;
+const sizes: HTMLElement = document.querySelector(".sizes")!;
+
 // Movement Animation Eventid
 container.addEventListener("mousemove", (e: MouseEvent): void => {
   let xAxis: number = (window.innerWidth / 2 - e.pageX) / 25;
@@ -12,12 +19,24 @@ container.addEventListener("mousemove", (e: MouseEvent): void => {
 // Animate In
 container.addEventListener("mouseenter", (e: MouseEvent): void => {
   card.style.transition = "none";
+  // Popout
+  title.style.transform = "translateZ(200px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(200px)";
+  purchase.style.transform = "translateZ(75px)";
 });
 
 // Animate Out
 container.addEventListener("mouseleave", (e: MouseEvent): void => {
   card.style.transition = "all 0.5s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  // Snapback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
 });
 
 
